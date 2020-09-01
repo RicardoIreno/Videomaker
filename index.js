e25eb658f002c2ac837c6e66f45d9f3ebddbba38
Ricardo Ireno <rilustra@outlook.com>
2020-09-01 15:36:29 -0300

    feat: add google-search. Problems with Apikay of Google Search

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 const robots = {
     input: require('./robots/input'),
     text: require('./robots/text.js'),
-    state: require('./robots/state.js')
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js')
 }
 
 
 async function start() {
 
-    robots.input()
-    await robots.text()
+    //robots.input()
+    // await robots.text()
+    await robots.image()
 
     const content = robots.state.load()
     console.dir(content, {depth: null} )
